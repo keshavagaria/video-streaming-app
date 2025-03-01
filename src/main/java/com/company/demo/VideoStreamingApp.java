4907818cfddcfbd325f0fc5b0370e0227547d7f9
Vishal Singh <vishal267s@gmail.com>
2025-03-01 16:15:00 +0530

    ok

--- src/main/java/com/company/demo/VideoStreamingApp.java
+++ src/main/java/com/company/demo/VideoStreamingApp.java
@@ -2,18 +2,20 @@ package com.company.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class VideoStreamingApp	
+public class VideoStreamingApp	extends SpringBootServletInitializer
 {
     public static void main( String[] args )
     {
     	SpringApplication.run(VideoStreamingApp.class, args);
     }
     
-//    @Override
-//	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
-//		// TODO Auto-generated method stub
-//		return builder.sources(VideoStreamingApp.class);
-//	}
+    @Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(VideoStreamingApp.class);
+	}
 }
