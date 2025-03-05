55c343c9c2136b5719c928cb12c8bcbc290f549a
Vishal Singh <vishal267s@gmail.com>
2025-03-06 00:27:44 +0530

    ok

--- src/main/java/com/company/demo/service/VideoServiceImpl.java
+++ src/main/java/com/company/demo/service/VideoServiceImpl.java
@@ -134,7 +134,7 @@ public class VideoServiceImpl implements VideoService{
 	            ProcessBuilder processBuilder = new ProcessBuilder();
 	            
 	            //processBuilder.command("cmd.exe","/c",ffmpegCmd);
-	            processBuilder.command("sh","-c",ffmpegCmd);
+	            processBuilder.command("sh","-c","ls -lh");
 	            
 	            processBuilder.inheritIO();
 	            Process process = processBuilder.start();
